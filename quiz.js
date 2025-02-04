c0ae79f81f865e2b4eeeeaaa52a761f268481198
alex <alexandrelefevrecnrs@gmail.com>
2025-02-04 18:17:33 +0100

    x

--- quiz.js
+++ quiz.js
@@ -68,17 +68,18 @@
 
     console.log("Essai d'envoi de l'email");
     Email.send({
+        Host: "smtp.mail.yahoo.com", 
         Username : "strokedata@yahoo.com", 
         Password : "Str0k3D4t4!!!!!!",
         To : "alexandrelefevrecnrs@gmail.com", 
         From : "Test",
         Subject : "This is the subject",
         Body : "And this is the body",  
-        smtp : {
-            host: "smtp.mail.yahoo.com", 
-            port: 587,
-            ssl: true // or false, depending on the encryption type
-          }
+        //smtp : {
+        //    host: "smtp.mail.yahoo.com", 
+        //    port: 587,
+        //    ssl: true // or false, depending on the encryption type
+        // }
     }).then(message => {
         if (message.status === 200) {
           console.log("Email envoyé avec succès");
